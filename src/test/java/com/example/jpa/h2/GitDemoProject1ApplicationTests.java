b7499b998ae30bb9c293924d353b162ddfc1deaf
Admin <Admin@DESKTOP-2TV55KJ>
2022-01-06 14:15:56 +0530

    first commit...

---
+++ src/test/java/com/example/jpa/h2/GitDemoProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
